f77e1f3f5e242afa560783fd95ca658b01800af9
laike9m <laike9m@gmail.com>
2014-05-27 22:51:36 +0800

    改变了数据库存储结构

--- nodehash/block_hash.js
+++ nodehash/block_hash.js
@@ -1,3 +1,15 @@
+/*
+存储格式
+{
+    {name: filename1, hashlist: [block1hash, block2hash, ...], hash: final_hash}
+    {name: filename2, hashlist: [block1hash, block2hash, ...], hash: final_hash}
+    ...
+}
+final_hash 由分块 hash 的结果连起来做 hash 生成
+TODO: 封装成函数, exports
+ */
+
+
 var xxhash = require('xxhash')
   , fs = require('fs')
   , path = require('path')
@@ -15,38 +27,40 @@ db.remove({}, {multi: true}, function(err, numRemoved) {
 });
 
 
-filepath = 'files/file1';
-
-var readable = fs.createReadStream(filepath),
+var filepath = 'files/file1',
+    readable = fs.createReadStream(filepath),
     filesize = fs.statSync(filepath)['size'],
-    M = 1024 * 1024, flag = 0;
+    M = 1024 * 1024,
+    flag = 0,
+    seed = 0xAAAA;
 
-function hash_then_insert(data, db, count) {
-    hash = xxhash.hash(data, 0xAAAABBB);
-    db.insert({
-        'index': count,
-        'hash': hash
-    }, function(err, newDoc) {
-        console.log(newDoc);
-    });
-}
-
-var count = 0, oneMdata, oneMhash;
+var count = 0, oneMdata, hashlist=[], hashstring='', final_hash;
 readable.on('readable', function() {
     while ((filesize - count * M > M)) {
         if (null != (oneMdata=readable.read(1024))) {  //异步读取
             count ++;
             flag = 1;
-            console.log(count);
-            hash_then_insert(oneMdata, db, count);
+            var hash = xxhash.hash(oneMdata, seed);
+            hashlist.push(hash);
+            hashstring += hash;
         }
     }
     if (flag) {  // 只要是readable的状态就会进入function, 所以必须限制使得读取完成之后回调函数不再起作用
         flag = 0;
         oneMdata = readable.read();
-        console.log(++count);
+        console.log('block count:' + ++count);
         console.log(oneMdata.length);
-        hash_then_insert(oneMdata, db, count);
+        var hash = xxhash.hash(oneMdata, seed);
+        hashlist.push(hash);
+        hashstring += hash;
+        final_hash = xxhash.hash(Buffer(hashstring), seed);
+        db.insert({
+            'filename': path.basename(filepath),
+            'hashlist': hashlist,
+            'hash': final_hash
+        }, function(err, newDoc) {
+            console.log(newDoc);
+        });
     }
 });
 
