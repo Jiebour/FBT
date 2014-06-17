bea22e57f555665483e36b0f4b78afea96d7fb43
laike9m <laike9m@gmail.com>
2014-06-17 14:08:37 +0800

    clean file-download folder

--- file-download/p2p-server3.js
+++ file-download/p2p-server3.js
@@ -1,51 +1,35 @@
-var networkutil=require("./network-util");
-
 var socket = require("net");
 var fileSystem = require("fs");
 var bson = require('buffalo');
 
+
+function indexOfSplitter(buffer){
+    var i=0,NOT_FOUND=-1;
+    while(i<(buffer.length-4)){
+        if(buffer[i]==0x40 && buffer[i+1]==0x40 && buffer[i+2]==0x40 && buffer[i+3]==0x40  && buffer[i+4]==0x40){
+            return i;
+        }
+        i+=1;
+    }
+    return NOT_FOUND;
+}
+    
 function hasFileIndex(jsonData){
     return "index" in jsonData;
 }
     
+var SPLITTER='@@@@@';
 var connectionCnt = 0, users = {};
 var dataToProcess=new Buffer(0);
 
 var server = socket.createServer(function (connection){
     var nickname;
-    //connection.setEncoding('binary');
-    ////connection.write(welcome());
-
-    //TODO
-    //add IP and user ID dection here
 
     connectionCnt++;
     connection.on('data', function (data){
         console.log("data received:"+data);
-//        if(!nickname) {//sending msg is username
-//            if(users[data]) {
-//                connection.write('\033[93m > nickname already in use. Try again:\033[39m ');
-//                return;
-//            } else {
-//                nickname = data;
-//                users[nickname] = connection;
-//    
-//                for(var i in users) {
-//                    users[i].write('\033[90m > ' + nickname + ' joined the room\033[39m\n');
-//                }
-//            }
-//        } else {//sending msg is normal chat message
-//            for(var i in users) {
-//                if(i != nickname) {
-//                    users[i].write('\033[96m > ' + nickname + ':\033[39m ' + data + '\n');
-//                }
-//            }
-//        }
-
-        ///var dataToProcess=Buffer.concat(chunks);
         dataToProcess=Buffer.concat([dataToProcess,data]);
-        //greedy
-        var index=networkutil.indexOfSplitter(dataToProcess);
+        var index=indexOfSplitter(dataToProcess);
         while(index>-1) {
             //bson is here
             console.log("receiving size:"+dataToProcess.slice(0, index).length);
@@ -64,7 +48,7 @@ var server = socket.createServer(function (connection){
                     var toSend=bson.serialize({header:"media",content:data});
                     console.log("data size:"+data.length+" bson size:"+toSend.length);
                     connection.write(toSend);
-                    connection.write(networkutil.SPLITTER);
+                    connection.write(SPLITTER);
                 });
                 
                 readStream.on('end', function() {
@@ -75,26 +59,19 @@ var server = socket.createServer(function (connection){
                 console.log("Waning: bson is not a file content...");
             }            
             
-            dataToProcess=dataToProcess.slice(index+networkutil.SPLITTER.length,dataToProcess.length);// Cuts off the processed chunk
-            index=networkutil.indexOfSplitter(dataToProcess);
+            dataToProcess=dataToProcess.slice(index+SPLITTER.length,dataToProcess.length);// Cuts off the processed chunk
+            index=indexOfSplitter(dataToProcess);
         }        
     });
     connection.on('close', function(){
         connectionCnt--;
         console.log('close....');
-        //delete users[nickname];
-        //connection.write('\033[90 > ' + nickname + ' left the room\033[39m\n');
-        //Object.keys(users).forEach(function (user) {
-        //users[user].write(nickname + ' left the room');
-        //});
     });
     connection.on('error', function(){
         console.log('\033[96m error occur.  \033[39m');
     }
     );
 });
-
-//exports.fileserver=server;
 
 server.listen(8803, function (){
     console.log('\033[96m   server listening on *:8803\033[39m');
