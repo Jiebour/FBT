2403f023260cfa6e5c5bfa8bd904bd1b5c73dd2e
laike9m <laike9m@gmail.com>
2014-06-30 15:41:56 +0800

    create app nat_download

---
+++ nat_download/p2p-server.js
@@ -0,0 +1,74 @@
+var dgram = require("dgram"),
+    fs = require("fs"),
+    bson = require('buffalo'),
+    utils = require('./utils'),
+    settings = require('./settings');
+
+var BLOCK_SIZE = settings.BLOCK_SIZE,
+    source_file = settings.source_file;
+
+var connectionCnt = 0;
+
+function addEventListener(socket) {
+    var toSend = new Buffer(0);
+    connectionCnt++;
+    socket.on('message', function (data, rinfo){
+//        console.log("data received:" + data.toString());
+
+            //bson is here
+//            console.log("receiving size:" + dataToProcess.slice(0, index).length);
+            //Process bson
+
+        var jsonData = bson.parse(data);
+        //has file content
+        if (utils.hasFileIndex(jsonData)) {
+            var blockID = jsonData["index"];
+            var readStream = fs.createReadStream(
+                source_file, {
+                    start: blockID * BLOCK_SIZE,
+                    end: blockID * BLOCK_SIZE + BLOCK_SIZE - 1,
+                    autoClose: true
+                }
+            );
+            readStream.on('data', function(data) {
+                console.log('transfer data....', blockID);
+                toSend = bson.serialize({header: "media", index: blockID, content: data});
+//                    console.log("data size:" + data.length + " bson size:" + toSend.length);
+                // 让client最后向每个server发一个结束信息, server收到结束信息之后把自己剩下的包发出去
+                // client那一边最后应当记录收到的block, 最后检查有哪些没收到, 然后请求这些剩余的块
+                socket.send(toSend, 0, toSend.length, rinfo.port, rinfo.address);
+            });
+
+            readStream.on('end', function () {
+//                    console.log('transfer data end....');
+            });
+        }
+        else{
+            console.log("Waning: bson is not a file content...");
+        }
+    });
+    socket.on('close', function(){
+        connectionCnt--;
+        console.log('close....');
+    });
+    socket.on('error', function(){
+        console.log('\033[96m error occur.  \033[39m');
+    });
+}
+
+var server1 = dgram.createSocket('udp4');
+var server2 = dgram.createSocket('udp4');
+var server3 = dgram.createSocket('udp4');
+
+server1.bind(8801, function () {
+    addEventListener(server1);
+    console.log('\033[96m   server listening on *:8801\033[39m');
+});
+server2.bind(8802, function (){
+    addEventListener(server2);
+    console.log('\033[96m   server listening on *:8802\033[39m');
+});
+server3.bind(8803, function (){
+    addEventListener(server3);
+    console.log('\033[96m   server listening on *:8803\033[39m');
+});
---
+++ nat_download/utils.js
@@ -0,0 +1,139 @@
+var fs = require('fs');
+var settings = require('./settings');
+var xxhash = require('xxhash');
+var crc = require('crc');
+
+
+function hasFileContent(jsonData){
+    return "content" in jsonData;
+}
+
+
+function hasFileIndex(jsonData){
+    return "index" in jsonData;
+}
+
+
+function rand3(){
+    //Return a random number between 1 and 3:
+    return Math.floor((Math.random() * 3) + 1);
+}
+
+
+function arrayEqual(a, b){
+    // assume a.length == b.length
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+
+function allOne(a) {
+    for (var i=0; i< a.length; i++) {
+        if (a[i] !== 1)
+            return false;
+    }
+    return true;
+}
+
+
+function countOne(a) {
+    var count = 0;
+    for (var i=0; i< a.length; i++) {
+        if (a[i] === 1)
+            count ++;
+    }
+    return count;
+}
+
+
+function diff_block(tobe_check, callback) {
+    if (tobe_check.length == 0) {
+        // 这种情况在interval未到但是已经校验完该part的block时出现
+        callback();
+        return;
+    }
+    var blocksize=settings.BLOCK_SIZE, source=settings.source_file, download=settings.download_file;
+    var totalblocks = parseInt((settings.filesize-1)/settings.BLOCK_SIZE);
+    var bf1 = Buffer(blocksize);
+    var bf2 = Buffer(blocksize);
+
+    function compare_block(readsize, i, fd1, fd2) {
+        // 有时候fd会莫名其妙地消失, 这时候只能重新调用diff_block生成fd1, fd2
+        try {
+            var block_index = tobe_check[i];
+            fs.read(fd1, bf1, 0, readsize, block_index * blocksize, function (err, bytesRead, bf1) {
+                fs.read(fd2, bf2, 0, readsize, block_index * blocksize, function (err, bytesRead, bf2) {
+                    var result;
+                    if (tobe_check[i] == totalblocks)
+                        result = (crc.crc32(bf1.slice(0, bytesRead)) === crc.crc32(bf2.slice(0, bytesRead)))
+                            ? 0 : 1;
+                    else
+                        result = (crc.crc32(bf1) === crc.crc32(bf2)) ? 0 : 1;
+                    if (result !== 0) {
+                        console.log("block ", block_index, " not equal!");
+                        // 校验未通过, 重新把block的下载记录置0, 之后会重新下载
+                    }
+                    else {
+                        tobe_check.splice(i, 1);
+//                        console.log("block ", block_index, " equal!");
+                    }
+                    if (i > 0) {
+                        // 考虑到splice对index的影响, 采用逆序递归
+                        compare_block(blocksize, i - 1, fd1, fd2);
+                    }
+                    else {
+                        callback();
+                    }
+                });
+            });
+        }
+        catch (e){
+            console.log(e.message)
+        }
+    }
+
+   if (tobe_check[tobe_check.length-1] == totalblocks)
+       compare_block(settings.filesize % blocksize, tobe_check.length-1, global.fd1, global.fd2);
+   else
+       compare_block(blocksize, tobe_check.length-1, global.fd1, global.fd2);
+}
+
+
+function addr2bytes(addr, nat_type_id) {
+    var host = addr.ip;  // "0.0.0.0"
+    var port = addr.port;  // int 23456
+    var bytes = Buffer(7);  // 和之前不同,现在用7个字节
+    var first4bytes = Buffer(host.split('.'));  // 用数字初始化
+    var byte5 = Math.floor(port/256);
+    var byte6 = port - byte5 * 256;
+    var next2bytes = Buffer([byte6, byte5]);
+    first4bytes.copy(bytes);
+    next2bytes.copy(bytes, 4);
+    // 因为id不会超过4, 所以写最后一个字节就行, 1=0x33, 注意是字符串不是Int
+    bytes.write(nat_type_id.toString(), 6);
+    return bytes;
+}
+
+function bytes2addr(bytes) {
+    var nat_type_id = bytes.readUInt8(6);  // 这是字符串不是Int
+    var ip = Array();
+    for (var i=0; i<4;i++) {
+        ip.push(bytes.readUInt8(i));
+    }
+    ip = ip.join('.');
+    var port = bytes.readUInt16LE(4);
+    return [ip, port, nat_type_id];
+}
+
+
+exports.bytes2addr = bytes2addr;
+exports.addr2bytes = addr2bytes;
+exports.print = console.log;
+exports.hasFileContent = hasFileContent;
+exports.hasFileIndex = hasFileIndex;
+exports.rand3 = rand3;
+exports.arrayEqual = arrayEqual;
+exports.allOne = allOne;
+exports.diff_block = diff_block;
