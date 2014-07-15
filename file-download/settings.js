afa9563c46c46981cdbc4db7688c90ead7b9918c
laike9m <laike9m@gmail.com>
2014-07-15 18:57:23 +0800

    move seed to settings

--- file-download/settings.js
+++ file-download/settings.js
@@ -5,7 +5,8 @@ var filepath1 = 'test.mp4',
     filesize = fs.statSync(filepath1)['size'],
     BLOCK_SIZE = 4096,  // bytes
     BLOCK_IN_PART = 1024,
-    server_ip = "127.0.0.1";
+    server_ip = "127.0.0.1",
+    seed = 0xAAAA;
 
 // delay time
 var unit_delay_time = 2000; // 该值最好和文件大小关联上
@@ -17,3 +18,4 @@ exports.BLOCK_SIZE = BLOCK_SIZE;
 exports.unit_delay_time = unit_delay_time;
 exports.BLOCK_IN_PART = BLOCK_IN_PART;
 exports.server_ip = server_ip;
+exports.seed = seed;
