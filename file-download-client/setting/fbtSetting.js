8cfac254bdad603bc0ad2d25627fe506c7c6cbd2
bonelee <lizhihua2000@gamil.com>
2014-07-17 00:49:13 +0800

    文件下载默认目录加入

--- file-download-client/setting/fbtSetting.js
+++ file-download-client/setting/fbtSetting.js
@@ -6,6 +6,7 @@
 var cleanup = require('../fbtUtils/cleanUp').Cleanup(myCleanup);
 var fs = require('fs');
 var path = require('path');
+var utils =require('../fbtUtils/fbtUtils');
 
 var config={SETTING_FILE: path.join(__dirname, 'setting.json')};//set to current path
 //var setting={};//not work
@@ -24,13 +25,22 @@ function myCleanup() {
 function loadSetting(){
   if (fs.existsSync(config.SETTING_FILE)) {
     var data = fs.readFileSync(config.SETTING_FILE);
-    setting = JSON.parse(data);
+    global.setting = JSON.parse(data);
     //global.log.info("loadSetting ok. settings: "+setting);
+  }else{//default download dir
+    var defaultDownloadDir=path.join(utils.getUserHome(),"FBTDownload");
+    global.setting["downloadSaveDir"]=defaultDownloadDir;
+    if(!fs.existsSync(defaultDownloadDir)){//mkdir if not exist
+        try{
+            fs.mkdirSync(defaultDownloadDir);
+        }catch(e) {//pass error
+        }
+    }
   }
 }
 
 function saveSetting(){
-  fs.writeFileSync(config.SETTING_FILE, JSON.stringify(setting, null, 2));
+  fs.writeFileSync(config.SETTING_FILE, JSON.stringify(global.setting, null, 2));
 }
 
 //exports.setting=setting;
