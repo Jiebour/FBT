57ffa97a9264029f1cb0cbd806fdcdc719f7d290
bonelee <lizhihua2000@gamil.com>
2014-07-17 22:01:39 +0800

    come on

--- file-download-client2/file-download-client/testFBT.js
+++
@@ -1,4 +0,0 @@
-var resourceShare=require('./shareResource/uploadResource.js');
-console.log("DB:%j",resourceShare.resourceDB);
-var httpServer=require('./fileDownloadV6/run.js');
-httpServer.start({fbtHost: '192.168.1.101', fbtPort: 8888, fbtUser: 'bone', resourceDB: resourceShare.resourceDB}); 
